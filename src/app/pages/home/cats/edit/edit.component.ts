b77be6b7167d023502b6b70aa0925455f3bb7159
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-18 12:35:33 -0500

    fixing details ✨

--- src/app/pages/home/cats/edit/edit.component.ts
+++ src/app/pages/home/cats/edit/edit.component.ts
@@ -6,21 +6,30 @@ import { CatsService } from 'src/app/services/cats.service';
 import { ImagesService } from 'src/app/services/images.service';
 import Swal from 'sweetalert2'
 
+
+interface Data {
+  data: FavoriteImage[]
+}
+
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  data!: { data: FavoriteImage[] };
+  images!: FavoriteImage[];
   constructor(
-    private route: ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     private imagesService: ImagesService,
     private catsService: CatsService,
     private router: Router
     ) {
-    this.route.params.subscribe(params => {
+    this.activatedRoute.params.subscribe(params => {
       this.catId = params['id'];
     });
+    this.data = this.activatedRoute.snapshot.data as Data;
+    this.images = this.data.data;
   }
   catForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required]),
@@ -29,7 +38,6 @@ export class EditComponent implements OnInit {
     image: new FormControl('', [Validators.required]),
   });
   catId: string = '';
-  images: FavoriteImage[] = [];
   favoriteImages: boolean = false;
 
   ngOnInit(): void {
@@ -40,16 +48,7 @@ export class EditComponent implements OnInit {
     this.update()
   }
 
-  async getCatById() {
-    const cat = await this.catsService.listById(this.catId);
-    this.catForm.get('name')?.setValue(cat.name)
-    this.catForm.get('breed')?.setValue(cat.breed)
-    this.catForm.get('age')?.setValue(cat.age)
-    this.catForm.get('image')?.setValue(cat.image)
-  }
-
   showFavoriteImages() {
-    this.listFavoriteImages()
     this.favoriteImages = true;
   }
 
@@ -57,8 +56,12 @@ export class EditComponent implements OnInit {
     this.catForm.get('image')?.setValue(url)
   }
 
-  async listFavoriteImages() {
-    this.images = await this.imagesService.favoriteImages()
+  async getCatById() {
+    const cat = await this.catsService.listById(this.catId);
+    this.catForm.get('name')?.setValue(cat.name)
+    this.catForm.get('breed')?.setValue(cat.breed)
+    this.catForm.get('age')?.setValue(cat.age)
+    this.catForm.get('image')?.setValue(cat.image)
   }
 
   async update() {
