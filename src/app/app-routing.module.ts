b0a2c7f7f3132d86b61487b2ed0dc901ec29fae4
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-17 21:16:48 -0500

    initial config :camera_flash:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { homeRoutes } from './pages/home/home.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent,
+    children:homeRoutes,    
+  },
+  { path:'',component:HomeComponent},
+  { path: '**', redirectTo: '' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
