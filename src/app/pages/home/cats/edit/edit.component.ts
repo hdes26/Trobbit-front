c7510b305d8e1b9d963010cb8b023ca0591e7501
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-18 00:24:37 -0500

    [ADD] show favorite images

--- src/app/pages/home/cats/edit/edit.component.ts
+++ src/app/pages/home/cats/edit/edit.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FavoriteImage } from 'src/app/models/favoriteImage.model';
+import { ImagesService } from 'src/app/services/images.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
-  constructor(private route: ActivatedRoute) {
+export class EditComponent implements OnInit{
+  constructor(private route: ActivatedRoute, private imagesService: ImagesService) {
     this.route.params.subscribe(params => {
       const catId = params['id'];
     });
+  }
+  ngOnInit(): void {
+    this.favoriteImages()
+  }
+  images:FavoriteImage[] = [];
+
+
+  async favoriteImages(){
+     this.images= await this.imagesService.favoriteImages()
   }
 }
